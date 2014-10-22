b5f8f838a17621e4c4d772bec2a76fe0e7c53bf2
yapkuantan <verbonius@yahoo.com>
2014-10-22 10:10:51 +0800

    run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
-## This file contains the functions to fulfil the tasks below: 
+## This file run_analysis.R contains the implementation steps to fulfil the tasks below: 
 ##1) Merges the training and the test sets to create one data set.
 ##2) Extracts only the measurements on the mean and standard deviation for each measurement. 
 ##3) Uses descriptive activity names to name the activities in the data set
@@ -9,7 +9,7 @@
 library(reshape2)
 
 ## set working directory
-setwd("C:/MOOC/3. Getting and Cleaning Data/Project")
+setwd("C:/getting-cleaning-data-project")
 
 ## Read outcome data
 activitylabels <- read.table("./UCI HAR Dataset/activity_labels.txt")[,2]
